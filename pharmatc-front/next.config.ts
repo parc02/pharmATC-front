ab4cac5facabec0d1d6b8f8a99d03cfaa08d1067
minseongpark <pascalmin49@gmail.com>
2025-06-24 08:08:57 +0900

    배포중

--- pharmatc-front/next.config.ts
+++ pharmatc-front/next.config.ts
@@ -1,10 +1,7 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     output: 'export',
-    images: {
-        unoptimized: true,
-    },
+    trailingSlash: true,
 };
 
 export default nextConfig;
