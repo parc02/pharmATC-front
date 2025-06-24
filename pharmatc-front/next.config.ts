35504537e7325003b50ac0218892d8da3745c40a
minseongpark <pascalmin49@gmail.com>
2025-06-24 16:02:03 +0900

    이미지 수정

--- pharmatc-front/next.config.ts
+++ pharmatc-front/next.config.ts
@@ -1,10 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
     output: 'export',
     trailingSlash: true,
     images: {
-        domains: ['pharmatc-backend-production.up.railway.app'], // 여기에 실제 이미지 도메인 추가
+        domains: [
+            'pharmatc-backend-production.up.railway.app',
+            'nedrug.mfds.go.kr', // 약 이미지 실제 도메인 추가
+        ],
     },
-};
+}
 
-export default nextConfig;
+export default nextConfig
